570dc69eee40d05e581bc351da632247ed360379
Boris Krasko <boris.krasko@icloud.com>
2020-10-02 02:01:18 +0200

    feat: pass dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	let arr = [];
+	if (Array.isArray(members)) {
+		for (let i = 0; i < members.length; i++) {
+			if (typeof members[i] === 'string') {
+				let j = 0;
+				while (members[i][j] == ' ') {
+					j++;
+				}
+				arr.push(members[i][j].toUpperCase());
+			}
+		}
+		arr = arr.sort().join(``);
+	}
+	return arr;
 };
