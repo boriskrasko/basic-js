87b6f9070e7e1e1b5f129e7db2a52350f4eecfe9
Boris Krasko <boris.krasko@icloud.com>
2020-10-29 23:02:35 +0100

    feat: completed!!!

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+  currentArray: [],
+    getLength() {
+      return this.currentArray.length;
+    },
+    addLink(value) { 
+        if (value === '') {
+          this.currentArray.push(`(  )`);
+        } 
+        this.currentArray.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+      if (typeof(position) !== "number") {
+        this.currentArray = [];
+        throw new Error();
+      } else {
+        this.currentArray = this.currentArray.slice(0, position - 1).concat(this.currentArray.slice(position)); 
+        return this;
+      }      
+    },
+    reverseChain() {
+      this.currentArray = this.currentArray.reverse();
+      return this;
+    },
+    finishChain() { 
+      let result = this.currentArray.join('~~');  
+      this.currentArray = [];  
+      return result;
+    }
 };
 
 module.exports = chainMaker;
